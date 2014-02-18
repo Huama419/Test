2d793a92ae421306742349b4e86bb861866bf05a
Huama419 <huama419@gmail.com>
2014-02-18 16:19:41 +0100

    pre-processor in Makefile

--- MakefieTest/hellofunc.c
+++ MakefieTest/hellofunc.c
@@ -1,7 +1,14 @@
 #include <stdio.h> 
 #include "hellomake.h"
 
+
 void myPrintHelloMake(void) {
+
+  #ifdef EXTRA
+    printf("EXTRA is set\n");
+  #else
+    printf("EXTRA is not set\n");
+  #endif
 
   printf("Hello makefiles!\n");
 
